0c0a5ac1b08f4a0322aaeb1e82ddbb13eaf23f90
Son Anh <sonanh@Sons-Mac-mini.local>
2026-06-05 20:31:35 +0700

    feat(mock-paypal): bootstrap Java 25 Spring Boot module

---
+++ mock-paypal-service/src/main/java/org/aibles/mockpaypal/MockPaypalApplication.java
@@ -0,0 +1,11 @@
+package org.aibles.mockpaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPaypalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MockPaypalApplication.class, args);
+    }
+}
